8bb002fe605a4a7c1d8b21d72070637f1e9b9aea
Sinchana M <100027844+sinchana0m@users.noreply.github.com>
2024-03-06 18:43:49 +0530

    Day1.py

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#Day1
+print("Hello Everyone")
+print('How are you?')
+print('"hello"\n')
+#Challenge of Day1
+print('Sinchana \n March 6th 2024 \n I am signing up for Replit\'s 100 days of Python challenge! \n 
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. \n 
+I\'ll be using Replit,an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field! \n 
+I am feeling 😊 \n
+You can follow my progress at replit.com/@sinchana0m ')
