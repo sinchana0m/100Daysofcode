185cc02070a822266914cbee90ced9e1cb80369f
Sinchana M <100027844+sinchana0m@users.noreply.github.com>
2024-03-07 21:19:03 +0530

    Day3.py
    This is the challenge solution for  Day 3 of 100 days code  covering the topic of concatination using python.

---
+++ Day3.py
@@ -0,0 +1,11 @@
+#day3 concatenation
+#challenge
+food=input("Name a food >")
+plant=input("Name a type of plant >")
+cooking=input("Name a method of cooking >")
+
+burned=input("What word describes burned food? >")
+item=input("Name a DIY item >")
+
+print("MENU")
+print(cooking,food,'with',plant,'on a bed of',item)
