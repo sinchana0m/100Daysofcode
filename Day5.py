1c314d4321a14b36a30bf970fa7c7ceba33dd383
Sinchana M <100027844+sinchana0m@users.noreply.github.com>
2024-03-11 10:05:56 +0530

    Day5.py

---
+++ Day5.py
@@ -0,0 +1,51 @@
+#Day 5
+ #common errors task solution
+#1 catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+ #if catsOrDogs == "cat":
+    #print("Meow")
+  #else:
+    #print("Woof")
+
+#2 catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+#if catsOrDogs == "cat":
+#  print("Meow")
+#else:
+#  print("Woof")
+
+#catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+#if catsOrDogs == "cat":
+#  print("Meow")
+#else:
+#  print("Woof")
+
+#task 2 FIX MY CODE
+#drink = input("Do you prefer coffee or tea?")
+#if drink =="coffee":
+#  print("Tea is better.")
+#else:
+#    print("Excellent choice.")
+
+#CHALLEGE WHICH CHARECTER ARE YOU GAME
+print('CREATE YOUR CHARECTER \n')
+print('---------------------------------')
+print('ANSWER THE QUESTIONS \n')
+hang=input("Do you like hanging around?: \n ")
+if hang =='yes':
+  print('You are spiderman')
+else:
+  print('Continue answering questions \n')
+tech=input('Are you tech wizard? : \n')
+if tech =='yes':
+  print('You are Iron man')
+else:
+  print('Continue for next questions \n')
+strong=input('Are you strong? : \n')
+if strong =='yes':
+  print("you are Hulk \n")
+else:
+  print('Continue for next questions \n')
+spy=input('Are you a spy? : \n')
+if spy =='yes':
+  print('You are black widow \n')
+else:
+  print('sorry we did not find your match better luck next time \n')
